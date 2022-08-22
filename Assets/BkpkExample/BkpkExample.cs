fb7ae1c644218e52558906d2c49e64661987eda5
Alex Patin <alex@mona.gallery>
2022-08-22 02:13:49 -0400

    Added side-by-side avatars in example

--- Assets/BkpkExample/BkpkExample.cs
+++ Assets/BkpkExample/BkpkExample.cs
@@ -14,7 +14,7 @@ namespace BkpkExample
 
         async void Test()
         {
-            Bkpk.AvatarInfo info = new Bkpk.AvatarInfo
+            Bkpk.AvatarInfo meebits = new Bkpk.AvatarInfo
             {
                 id = "XXX",
                 uri = "https://cdn.mona.gallery/e6db7ec3-d1c8-4935-86ce-b339a5d11eb6.vrm",
@@ -24,7 +24,21 @@ namespace BkpkExample
                 metadata = "",
             };
 
-            Bkpk.BkpkAvatar avatar = await Bkpk.Avatars.LoadAvatar(info);
+            Bkpk.AvatarInfo readyplayerme = new Bkpk.AvatarInfo
+            {
+                id = "XXX",
+                uri =
+                    "https://d1a370nemizbjq.cloudfront.net/0dccee22-f9db-44ca-a49d-deb8ca27aae5.glb",
+                format = "glb",
+                type = "humanoid",
+                provider = "readyplayerme",
+                metadata =
+                    "{\"bodyType\": \"fullbody\",\"outfitGender\": \"masculine\",\"outfitVersion\": 2}",
+            };
+
+            Bkpk.BkpkAvatar meebitsAvatar = await Bkpk.Avatars.LoadAvatar(meebits);
+            Bkpk.BkpkAvatar rpmAvatar = await Bkpk.Avatars.LoadAvatar(readyplayerme);
+            rpmAvatar.AvatarObject.transform.localPosition = new Vector3(2, 0, 0);
         }
     }
 }
