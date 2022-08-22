43bd262f8624d04abb957cbdf529f59ce8fce6da
Alex Patin <alex@mona.gallery>
2022-08-22 00:27:53 -0400

    Added Bkpk Example prefab

--- Assets/BkpkExample/BkpkExample.cs
+++ Assets/BkpkExample/BkpkExample.cs
@@ -7,11 +7,24 @@ namespace BkpkExample
 {
     public partial class BkpkExample : MonoBehaviour
     {
-        IEnumerator Init()
+        void Start()
         {
-            StartCoroutine(Test());
+            Test();
         }
 
-        async Task Test() { }
+        async void Test()
+        {
+            Bkpk.AvatarInfo info = new Bkpk.AvatarInfo
+            {
+                id = "XXX",
+                uri = "https://cdn.mona.gallery/e6db7ec3-d1c8-4935-86ce-b339a5d11eb6.vrm",
+                format = "vrm",
+                type = "humanoid",
+                provider = "meebits",
+                metadata = "",
+            };
+
+            Bkpk.BkpkAvatar avatar = await Bkpk.Avatars.LoadAvatar(info);
+        }
     }
 }
