e84de7a034af8ea87e6a91964a1781fdcb305ebf
Alex Patin <alex@mona.gallery>
2022-09-14 01:51:54 -0400

    Modified build settings

--- Assets/BkpkExample/BkpkExample.cs
+++ Assets/BkpkExample/BkpkExample.cs
@@ -9,20 +9,24 @@ namespace BkpkExample
     {
         void Start()
         {
-            Test();
-        }
-
-        async void Test()
-        {
             Bkpk.Config.WebSdkUrl = "https://owner.com.ngrok.io/JsSdk/dist/index.js";
             Bkpk.Config.BkpkApiUrl = "http://localhost:3000";
             Bkpk.Config.BkpkUrl = "http://localhost:8080";
-            Bkpk.Auth.Instance.AccessToken =
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIweGVkOThiMjZhNTgwZmFjY2VhYmU2YjVkMGJjZjZiYTM5OTJjMzRhZmYiLCJiYWNrcGFjayI6Ijc4YmQ2YmFjLThiODEtNDc1Zi1iMWMxLWQ0MDhhOTViZjAwYiIsInNjb3BlcyI6ImF2YXRhcnM6cmVhZCIsImlhdCI6MTY2MzEzMjU5OSwiZXhwIjoxNjYzMTMzNDk5fQ.X-RcJtTZiWPrb0xZb10onR7udiyO7nV-xxKpgSpF_qY";
+        }
 
+        async void OnAuthorized(string token)
+        {
             Bkpk.AvatarInfo avatar = await Bkpk.Avatars.GetDefaultAvatar();
 
             await Bkpk.Avatars.LoadAvatar(avatar);
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Bkpk.Auth.Instance.RequestAuthorization(OnAuthorized);
+            }
         }
     }
 }
