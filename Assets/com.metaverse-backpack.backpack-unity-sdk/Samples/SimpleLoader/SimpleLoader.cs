9de456819ab4da3c9ab834f55d481bbc7476d5ca
Alex Patin <alex@mona.gallery>
2022-09-26 13:48:53 -0400

    Fixed Sample

--- Assets/com.metaverse-backpack.backpack-unity-sdk/Samples/SimpleLoader/SimpleLoader.cs
+++ Assets/com.metaverse-backpack.backpack-unity-sdk/Samples/SimpleLoader/SimpleLoader.cs
@@ -9,63 +9,14 @@ namespace SimpleLoader
     {
         bool Initialized = false;
 
-        void Start()
-        {
-            Bkpk.Config.WebSdkUrl = "https://owner.com.ngrok.io/JsSdk/dist/index.js";
-            Bkpk.Config.BkpkApiUrl = "http://localhost:3000";
-            Bkpk.Config.BkpkUrl = "http://localhost:8080";
-            Bkpk.Config.ClientID = "20a3c6c5-0dbd-407e-9e54-d0d5cd31a6cf";
-            Bkpk.Config.IpfsGateway = "https://ipfs.mona.gallery";
-
-            OnAuthorized("XXX");
-        }
-
         async void OnAuthorized(string token)
         {
-            Bkpk.AvatarInfo rpm = new Bkpk.AvatarInfo
-            {
-                id = "XXX",
-                content = "bafybeiezpxc6nrxe6hdmlo7gmi46aigt24vhkt233qigvnlfybo7hznjl4",
-                metadata = new Bkpk.AvatarMetadata
-                {
-                    source = "XXX",
-                    fileFormat = "glb",
-                    type = "humanoid"
-                }
-            };
-
-            Bkpk.AvatarInfo meebits = new Bkpk.AvatarInfo
-            {
-                id = "XXX",
-                content = "bafybeibphpz3umc7s66zxoyo5why2rdgrnvo3wzcyj64ktt5ssvzob67vm",
-                metadata = new Bkpk.AvatarMetadata
-                {
-                    source = "XXX",
-                    fileFormat = "vrm",
-                    bodyType = "humanoid",
-                    type = "humanoid"
-                }
-            };
-
-            Bkpk.AvatarInfo ca = new Bkpk.AvatarInfo
-            {
-                id = "XXX",
-                content = "bafybeiglu4e4grhzw5kzek4epi32jqugki5o6q7kzh4jcx2icctowuiyva",
-                metadata = new Bkpk.AvatarMetadata
-                {
-                    source = "XXX",
-                    fileFormat = "vrm",
-                    bodyType = "humanoid",
-                    type = "humanoid"
-                }
-            };
-
-            await Bkpk.Avatars.LoadAvatar(rpm);
+            Bkpk.AvatarInfo avatar = await Bkpk.Avatars.GetDefaultAvatar();
+            await Bkpk.Avatars.LoadAvatar(avatar);
         }
 
         void Update()
         {
-            return;
             if (Input.GetMouseButton(0) && !Initialized)
             {
                 Bkpk.Auth.Instance.RequestAuthorization(OnAuthorized);
